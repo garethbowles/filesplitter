4ec6fd75c0452ff2597b54cde48f943d3458f135
Gareth Bowles <gareth@optimalops.net>
2015-03-14 20:50:40 -0700

    Initial version

---
+++ FileSplitter.groovy
@@ -0,0 +1,28 @@
+def inputFile = new File('pairs.txt')
+def valueLists = [:] // Maps key to a list of values for that key
+
+inputFile.eachLine { line ->
+    def fields = line.split(",")
+    if (fields.length > 2) {
+        println "Invalid line ${line}: should contain 2 values separated by a comma."
+    } else {
+        def fileKey = fields[0]
+        // Create a new list if there is no map entry for this key
+        valueLists[fileKey] ?: valueLists.put(fileKey, [])
+        // Add value to list
+        valueLists[fileKey] << fields[1]
+    }   
+}
+
+valueLists.each { valueMap ->
+    def fileKey = valueMap.key
+    // Create a new file for each key
+    def outFile = new File("pairs_${fileKey}")
+    // newWriter() overwrites the existing file
+    def w = outFile.newWriter()
+    // Append each key,value pair
+    valueMap.value.each { value ->
+        w << "${fileKey},${value}\n"
+    }
+    w.close()    
+}
