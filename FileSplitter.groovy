7a3842e80ccbc4fd2f1a136de1dee78e574e2577
Gareth Bowles <gareth@optimalops.net>
2015-03-15 15:50:52 -0700

    More logging and cleanup

--- FileSplitter.groovy
+++ FileSplitter.groovy
@@ -1,4 +1,16 @@
-def inputFile = new File('pairs.txt')
+/*
+  Split a file containing one key,value pair per line into one file of key,value
+  pairs for each unique key
+
+  TODO: 
+
+  1. Pass the input file name as a parameter
+  2. Use buffered input / output streams instead of reading the whole input file.
+
+*/ 
+
+def INPUT_FILE_NAME = 'pairs.txt'
+def inputFile = new File(INPUT_FILE_NAME)
 def valueLists = [:] // Maps key to a list of values for that key
 
 inputFile.eachLine { line ->
@@ -17,10 +29,10 @@ inputFile.eachLine { line ->
 valueLists.each { valueMap ->
     def fileKey = valueMap.key
     // Create a new file for each key
-    def outFile = new File("pairs_${fileKey}")
-    // newWriter() overwrites the existing file
+    def outFile = new File("${INPUT_FILE_NAME}_${fileKey}")
+    // newWriter() overwrites existing file
     def w = outFile.newWriter()
-    println "Created new file for key ${fileKey}..."
+    println "Created new file ${outFile.name} for key ${fileKey}..."
     // Append each key,value pair
     valueMap.value.sort().each { value ->
         w << "${fileKey},${value}\n"
